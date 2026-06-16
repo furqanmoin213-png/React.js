1e0f984699f09fe3577e20b5b80c9f1b8395b309
115355-lang <furqanmoin213@gmail.com>
2026-06-16 10:59:41 +0500

    Update ImageContainer.jsx

--- SACP/Login-SignUp-Page/src/Shared/ImageContainer.jsx
+++ SACP/Login-SignUp-Page/src/Shared/ImageContainer.jsx
@@ -1,5 +1,4 @@
 import Login_Illustration from "../Assets/Login_Illustration.png"
-import image from "../Assets/SignUp_Illustration.png"
 const ImageContainer = () => {
   return (
     <div className="h-screen w-full hidden lg:block">
@@ -8,11 +7,5 @@ const ImageContainer = () => {
   )
 }
 
-export const ImageContainer2=()=>{
-  return (
-    <div className="h-full w-full hidden lg:block bg-Page-background">
-      <img src={image} alt="Login Illustration" className="  outline-none border-none h-full w-full" />
-    </div>
-  )
-}
+
 export default ImageContainer;
